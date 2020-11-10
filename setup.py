5577d85e69912cd8d7f47124416c01f04c8a234c
Neel Gala <neelgala@incoresemi.com>
2020-11-10 17:03:58 +0000

    bump to 0.2.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [ ]
 
 setup(
     name='riscv_ctg',
-    version='0.1.0',
+    version='0.2.0',
     description="RISC-V CTG",
     long_description=readme + '\n\n',
     classifiers=[
