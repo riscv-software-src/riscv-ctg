22a46fbca905ed18fef6297d14a5b6b107719718
Muhammad Bilal Sakhawat <bilal.sakhawat@10xengineers.ai>
2021-09-20 17:14:46 +0500

    Bump version: 0.5.6 → 0.5.7

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [ ]
 
 setup(
     name='riscv_ctg',
-    version='0.5.6',
+    version='0.5.7',
     description="RISC-V CTG",
     long_description=readme + '\n\n',
     classifiers=[
