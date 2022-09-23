599a41a10e5d5425356c9ad697d206295a544f47
Weiwei Li <liweiwei@iscas.ac.cn>
2022-09-23 18:45:05 +0800

    Fix NameError: name 'size' is not defined Fix ValueError: invalid literal for int() with base 10

--- riscv_ctg/generator.py
+++ riscv_ctg/generator.py
@@ -793,13 +793,13 @@ class Generator():
                     if self.fmt in ['jformat','bformat'] or instr['inst'] in \
                         ['c.beqz','c.bnez','c.jal','c.j','c.jalr']:
                         var_dict['imm_val'] = \
-                            (-1 if instr['label'] == '1b' else 1) * int(instr['imm_val'])
+                            (-1 if instr['label'] == '1b' else 1) * int(instr['imm_val'], 0)
                     else:
-                        var_dict['imm_val'] = int(instr['imm_val'])
+                        var_dict['imm_val'] = int(instr['imm_val'], 0)
                 elif key == 'rm_val':
                     var_dict['rm_val'] = int(rm_dict[instr['rm_val']])
                 else:
-                    var_dict[key] = int(instr[key])
+                    var_dict[key] = int(instr[key], 0)
             for key in self.op_vars:
                 var_dict[key] = instr[key]
 
@@ -1164,7 +1164,7 @@ class Generator():
         if self.operation:
             for i in range(len(instr_dict)):
                 for var in self.val_vars:
-                    locals()[var]=int(instr_dict[i][var])
+                    locals()[var]=int(instr_dict[i][var], 0)
                 correctval = eval(self.operation)
                 instr_dict[i]['correctval'] = str(normalise(correctval,instr_dict[i]))
         else:
@@ -1201,8 +1201,9 @@ class Generator():
                     field != 'val_section' and field != 'val_offset' and field != 'rm_val':
                     value = instr_dict[i][field]
                     if '0x' in value:
-                        value = '0x' + value[2:].zfill(int(self.xlen/4))
-                        value = struct.unpack(size, bytes.fromhex(value[2:]))[0]
+#                        value = '0x' + value[2:].zfill(int(self.xlen/4))
+#                        value = struct.unpack(size, bytes.fromhex(value[2:]))[0]
+                        value = int(value, 16)
                     else:
                         value = int(value)
 #                    value = '0x' + struct.pack(size,value).hex()
