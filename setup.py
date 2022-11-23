a4037993c1f69ffda5629b61168310345f42e569
S Pawan Kumar <pawan.kumar@incoresemi.com>
2022-11-23 16:31:14 +0530

    Bump version: 0.10.2 → 0.10.3

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [ ]
 
 setup(
     name='riscv_ctg',
-    version='0.10.2',
+    version='0.10.3',
     description="RISC-V CTG",
     long_description=readme + '\n\n',
     classifiers=[
